2cc26cf30d2e3b737b9af2f431bbb9c27af10692
christzenleonardy <bookbukushu@gmail.com>
2018-09-12 21:10:17 +0700

    stupido

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ DigitalOut myled(LED1);
 
 int main() {
     while(1) {
-        myled = 1; // LED is ON
-        wait(0.2); // 200 ms
+        myled = 0; // LED is OFF
+        wait(1.5); // 1 sec
+        myled = 1; // LED is OFF
+        wait(0.5); // 1 sec
         myled = 0; // LED is OFF
         wait(1.5); // 1 sec
         myled = 1; // LED is OFF
